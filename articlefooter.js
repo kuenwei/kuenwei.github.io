446fa1c67c4b7504af95ab78a79c29a6c4eff251
kuenwei <34588711+kuenwei@users.noreply.github.com>
2017-12-31 11:04:47 +0800

    Article Footer

---
+++ articlefooter.js
@@ -0,0 +1,2 @@
+document.write('<script type="text/javascript" src="https://sites.google.com/site/anasblogsite/contentfooter.js"></script>');
+document.write('<script type="text/javascript" src="https://sites.google.com/site/anasblogsite/servicearea.js"></script>');
