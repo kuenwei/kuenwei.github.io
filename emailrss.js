c2ecc65f2ce2a43e60193d9e2bec4bfd49774e9d
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-01-19 23:25:21 +0800

    Update emailrss.js

--- emailrss.js
+++ emailrss.js
@@ -1,2 +1,3 @@
 document.writeln('<form style="border:1px solid #ccc;padding:3px;text-align:center;" action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open(\'https://feedburner.google.com/fb/a/mailverify?uri=willliu\', \'popupwindow\', \'scrollbars=yes,width=550,height=520\');return true">');
-document.writeln('<p>輸入你的電子郵件地址：</p><p><input type="text" style="width:140px" name="email"/></p><input type="hidden" value="willliu" name="uri"/><input type="hidden" name="loc" value="en_US"/><input type="submit" value="訂閱" /><p>發送者為 <a href="https://feedburner.google.com" target="_blank">FeedBurner</a></p></form>');
+document.writeln('<p>輸入你的電子郵件地址：</p><p><input type="text" style="width:140px" name="email"/><br /><input type="hidden" value="willliu" name="uri"/><input type="hidden" name="loc" value="en_US"/><input type="submit" value="訂閱" />');
+	document.writeln('</form>');
