f831a2bfb787cfdb7c0fc114b8202721b9be5a2b
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-01-19 23:48:04 +0800

    Update articlefooter.js

--- articlefooter.js
+++ articlefooter.js
@@ -1,3 +1,3 @@
-document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/emailrss.js"></script>');
 document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/contentfooter.js"></script>');
+document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/emailrss.js"></script>');
 document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/servicearea.js"></script>');
