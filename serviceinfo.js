ab3c57e6421dab102ad77d04c201bb17029908cc
Will Liu <kuenwei@gmail.com>
2018-02-25 14:20:15 +0800

    update

--- serviceinfo.js
+++ serviceinfo.js
@@ -6,7 +6,7 @@
 var service == "txtinfo";
 
 document.writeln('<div align="center">');
-document.writeln(loadservice(service));
+document.writeln('<a href="http://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a>｜<a href="https://docs.google.com/forms/d/e/1FAIpQLSeKNMurc7u7NtPQTRmMjJQarSxW6VGTCaN7u2C6McqO2rgDxw/viewform" target="_blank">預約諮商服務</a>｜<a href="http://grz.fyi/zT5k7N" target="_blank">關於我的完整介紹</a>');
 document.writeln('</div>');
 
 function loadservice( chks ) {
