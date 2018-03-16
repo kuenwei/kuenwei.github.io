a898bfd4873e612b97662ecaa9614817915ef2c2
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-03-16 21:06:41 +0800

    Update serviceinfo.js

--- serviceinfo.js
+++ serviceinfo.js
@@ -5,6 +5,6 @@
 
 document.writeln('<div align="center">');
 document.write('<a href="http://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a>｜');
-document.write('<a href="https://docs.google.com/forms/d/e/1FAIpQLSeKNMurc7u7NtPQTRmMjJQarSxW6VGTCaN7u2C6McqO2rgDxw/viewform" target="_blank">預約諮商服務</a>｜');
+document.write('<a href="http://grz.fyi/fbmewill" target="_blank">預約諮商服務(Facebook 訊息)</a>｜');
 document.write('<a href="http://grz.fyi/zT5k7N" target="_blank">關於我的完整介紹</a>');
 document.writeln('</div>');
