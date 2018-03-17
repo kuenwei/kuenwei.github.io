4472cbc6a031bd02f1523142ad644c52ca9f70db
Will Liu <kuenwei@gmail.com>
2018-03-17 21:32:05 +0800

    Updaet Service Info

--- blog_mlb/eoclassposts.js
+++ blog_mlb/eoclassposts.js
@@ -5,6 +5,6 @@
 
 document.writeln('<div align="center">');
 document.write('<i class="fa fa-calendar" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a>｜');
-document.write('<i class="fa fa-comments" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/fbmewill" target="_blank">預約諮商服務(FB Messenger)</a>｜');
+document.write('<i class="fa fa-comments" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/fbmewill" target="_blank">預約諮商服務(Facebook 訊息)</a>｜');
 document.write('<i class="fa fa-user aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/zT5k7N" target="_blank">關於我的完整介紹</a>');
 document.writeln('</div>');
