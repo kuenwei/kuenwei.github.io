6d91fc70c0ea1eaba75d3d5e22bd36294b10443b
Will Liu <kuenwei@gmail.com>
2018-03-17 21:40:58 +0800

    update eoclasspost.js

--- blog_mlb/eoclassposts.js
+++ blog_mlb/eoclassposts.js
@@ -3,8 +3,29 @@
 
 //document.write(service);
 
-document.writeln('<div align="center">');
-document.write('<i class="fa fa-calendar" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a>｜');
-document.write('<i class="fa fa-comments" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/fbmewill" target="_blank">預約諮商服務(Facebook 訊息)</a>｜');
-document.write('<i class="fa fa-user aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://grz.fyi/zT5k7N" target="_blank">關於我的完整介紹</a>');
-document.writeln('</div>');
+//document.writeln('<ul>');
+//document.write('<li><a href="https://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a></i>');
+//document.writeln('<ul>');
+
+//<!-- Recent Posts by Label Start -->
+//<!-- code by BloggerSentral.com -->
+
+function recentpostslist(json) {
+ document.write('<ul>');
+ for (var i = 0; i < json.feed.entry.length; i++)
+ {
+    for (var j = 0; j < json.feed.entry[i].link.length; j++) {
+      if (json.feed.entry[i].link[j].rel == 'alternate') {
+        break;
+      }
+    }
+var entryUrl = "'" + json.feed.entry[i].link[j].href + "'";//bs
+var entryTitle = json.feed.entry[i].title.$t;
+var item = "<li>" + "<a href="+ entryUrl + '" target="_blank">' + entryTitle + "</a> </li>";
+ document.write(item);
+ }
+ document.write('</ul>');
+ }
+
+document.writeln('<script src="'+location.protocol+"//"+location.hostname+'/feeds/posts/summary/-/'+listfor+'?max-results=100&alt=json-in-script&callback=recentpostslist"></script>');
+//document.writeln('<span style="font-size: 80%; float:right;">Get this <a href="http://www.bloggersentral.com/2010/04/list-recent-posts-by-label.html">widget</a></span>');
