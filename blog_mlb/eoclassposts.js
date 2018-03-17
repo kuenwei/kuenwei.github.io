525c7b50711889e21189117a8350bed155fb695a
Will Liu <kuenwei@gmail.com>
2018-03-17 21:56:57 +0800

    update title

--- blog_mlb/eoclassposts.js
+++ blog_mlb/eoclassposts.js
@@ -3,7 +3,7 @@
 
 //document.write(service);
 
-document.writeln('<p>您可以參考更多關於「精油抓周」相關的文章如下：</p>');
-document.writeln('<ul>');
+document.writeln('<h4>您可以參考更多關於「精油抓周」相關的文章如下：</h4>');
+document.writeln('<ul><h4>您可以參考更多關於「精油抓周」相關的文章如下：</h4>');
 document.write('<li><a href="https://grz.fyi/erFpXw" target="_blank">[精油抓周-1] 為什麼抓周油的味道變了？</a></i>');
 document.writeln('</ul>');
