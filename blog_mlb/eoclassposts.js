8babae9755d4f3cb5376010cb29a817e89fbfb00
Will Liu <kuenwei@gmail.com>
2018-03-17 21:54:36 +0800

    update eoclassposts.js

--- blog_mlb/eoclassposts.js
+++ blog_mlb/eoclassposts.js
@@ -4,5 +4,5 @@
 //document.write(service);
 
 document.writeln('<ul>');
-document.write('<li><a href="https://grz.fyi/UuwhSP" target="_blank">最新課程訊息</a></i>');
-document.writeln('<ul>');
+document.write('<li><a href="https://grz.fyi/erFpXw" target="_blank">[精油抓周-1] 為什麼抓周油的味道變了？</a></i>');
+document.writeln('</ul>');
