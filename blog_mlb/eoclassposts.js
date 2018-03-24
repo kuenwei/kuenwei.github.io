7433a824cce7e266ced23420c1b83d03523e036f
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-03-25 03:11:37 +0800

    Update eoclassposts.js

--- blog_mlb/eoclassposts.js
+++ blog_mlb/eoclassposts.js
@@ -6,4 +6,5 @@
 document.writeln('<h4>您可以參考更多關於「精油抓周」相關的文章如下：</h4>');
 document.writeln('<ul style="margin-top: 0; padding-top:0;">');
 document.write('<li><a href="https://grz.fyi/erFpXw" target="_blank" style="text-decoration: none;">[精油抓周-1] 為什麼抓周油的味道變了？</a></i>');
+document.write('<li><a href="https://grz.fyi/SJWdZV" target="_blank" style="text-decoration: none;">[精油抓周-2] 21 天後我要用什麼</a></i>');
 document.writeln('</ul>');
