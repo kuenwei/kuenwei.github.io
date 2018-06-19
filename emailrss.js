b0df948c2d3ac16b93ec4a1b7517176e99b7dd32
Will Liu <kuenwei@gmail.com>
2018-06-19 20:54:07 +0800

    update

--- emailrss.js
+++ emailrss.js
@@ -1,5 +1,5 @@
 document.writeln('<!--<form style="border:1px solid #ccc;padding:4px;text-align:center;" action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open(\'https://feedburner.google.com/fb/a/mailverify?uri=willliu\', \'popupwindow\', \'scrollbars=yes,width=550,height=520\');return true">');
-document.writeln('<h4>輸入 Email 訂閱最新文章與課程通知</h4><input type="text" style="width:65%; height: 40px; border: 0px; border-bottom: 1px solid; margin-bottom: 12px;" name="email"/>&nbsp;<input type="hidden" value="willliu" name="uri"/><input type="hidden" name="loc" value="en_US"/><input type="submit" value="訂閱" style="width:30%; height: 40px; border: 0px; background-color: #1688b8; color: #fff;" />');
+document.writeln('<h4>輸入 Email 訂閱最新文章與課程通知 </h4><input type="text" style="width:65%; height: 40px; border: 0px; border-bottom: 1px solid; margin-bottom: 12px;" name="email"/>&nbsp;<input type="hidden" value="willliu" name="uri"/><input type="hidden" name="loc" value="en_US"/><input type="submit" value="訂閱" style="width:30%; height: 40px; border: 0px; background-color: #1688b8; color: #fff;" />');
 document.writeln('</form>-->');
 
 document.writeln('<iframe style="height:150px;width:100%;max-width:800px;margin:30px auto; border: 1px solid #DDD;" src="https://upscri.be/af9988?as_embed"></iframe>');
