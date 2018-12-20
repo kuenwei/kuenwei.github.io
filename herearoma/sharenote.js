5f5f0a260eeefda094d7f96a260ac7019d30adf4
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-12-20 16:12:14 +0800

    Update sharenote.js

--- herearoma/sharenote.js
+++ herearoma/sharenote.js
@@ -1,4 +1,4 @@
-document.writeln("<div style=\"background-color: #ededed; line-height: 1.5em也; font-size: 1.1rem; padding: 16px 16px 16px 16px; margin: 8px 0px 8px 0px;\">");
+document.writeln("<div style=\"background-color: #ededed; line-height: 1.5em; font-size: 1.1rem; padding: 16px 16px 16px 16px; margin: 8px 0px 8px 0px;\">");
 document.write("如果您喜歡這篇文章，除了按讚、拍手，也歡迎分享到自己的社群中，文章採用<a href=\"https://creativecommons.org/licenses/by-nc-sa/3.0/tw/\" target=\"_blank\">創用 CC 姓名標示-非商業性-相同方式分享 3.0 台灣 授權條款</a>授權，");
 document.write("分享、引用請依授權規定，並附上原文連結：");
 //document.write("<a href=\""+location.href+"?utm_source=blogshare\" target=\"_blank\">");
