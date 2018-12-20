f0a2b734e8982ccba2fdcdec1e3ad4e24efd8981
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-12-20 16:56:39 +0800

    Update contact.js

--- herearoma/contact.js
+++ herearoma/contact.js
@@ -1,6 +1,6 @@
 document.writeln ('<h3>聯絡我們</h3>');
-document.writeln ('<ul style="white-space: nowrap; width:100%;">');
-document.writeln ('<li style="list-style-type: none; display: inline;"><a href="mailto:service@herearoma.com" style="background-color:#dd4b39; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">Email</a></li>');
-document.writeln ('<li style="list-style-type: none; display: inline;"><a href="https://he2.tw/fbpage" style="background-color:#3b5998; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">FB 粉絲專頁</a></li>');
-document.writeln ('<li style="list-style-type: none; display: inline;"><a href="https://he2.tw/lineat" style="background-color:#00c300;; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">加入 LINE 好友</a></li>');
+document.writeln ('<ul>');
+document.writeln ('<li><a href="mailto:service@herearoma.com" style="background-color:#dd4b39; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">Email</a></li>');
+document.writeln ('<li><a href="https://he2.tw/fbpage" style="background-color:#3b5998; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">FB 粉絲專頁</a></li>');
+document.writeln ('<li><a href="https://he2.tw/lineat" style="background-color:#00c300;; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">加入 LINE 好友</a></li>');
 document.writeln ('</ul>');
