a534b94831e253b08d780dfa7910dad0683b9121
kuenwei <34588711+kuenwei@users.noreply.github.com>
2018-12-20 11:06:42 +0800

    Update contact.js

--- herearoma/contact.js
+++ herearoma/contact.js
@@ -1,6 +1,2 @@
-document.writeln ('<h3>聯絡我們</h3>');
-document.writeln ('<ul>');
-document.writeln ('<li>Email：service@herearoma.com</li>');
-document.writeln ('<li>Facebook：<a href="https://he2.tw/fbpage" style="background-color:#3b5998; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">點此前往粉絲專頁</a></li>');
-document.writeln ('<li>LINE：<a href="https://he2.tw/lineat" style="background-color:#00c300;; color: #fff; border-radius:1em; padding: 0.3em; padding-left: 0.8em; padding-right: 0.8em; font-size: 0.9em;">點此加入好友</a></li>');
-document.writeln ('</ul>');
+
+document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/contentfooter.js"></script>');
