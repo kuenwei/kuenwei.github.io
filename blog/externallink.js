f2d57c4ed1c96b859070f0bf478628a952644922
Will Liu <kuenwei@gmail.com>
2018-12-23 17:39:47 +0800

    update blog js

---
+++ blog/externallink.js
@@ -0,0 +1,5 @@
+document.writeln('<div align="center">');
+document.write('<i class="fa fa-calendar" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://he2.tw/classes" target="_blank">最新課程訊息</a>｜');
+document.write('<i class="fa fa-comments" aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://he2.tw/service" target="_blank">服務說明與預約</a>｜');
+document.write('<i class="fa fa-user aria-hidden="true" style="margin-left: 0px; margin-right: 3px;"></i><a href="https://willliu.me" target="_blank">關於我的完整介紹</a>');
+document.writeln('</div>');
---
+++ blog/footer.js
@@ -0,0 +1,3 @@
+document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/blog/sharenote.js"></script>');
+document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/blog/externallink.js"></script>');
+document.writeln('<script type="text/javascript" src="https://kuenwei.github.io/blog/subscribe.js"></script>');
